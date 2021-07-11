753878dd61312a757da345f40137a932106ac1f6
RAJ <rajkumarkanjo@gmail.com>
2021-07-11 18:43:10 +0530

    local practice - 01

---
+++ QueueMain.java
@@ -0,0 +1,22 @@
+package DSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueueMain {
+
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        System.out.println(integers.size());
+
+
+    }
+}
+
+class MyQueue{
+
+
+
+
+}
